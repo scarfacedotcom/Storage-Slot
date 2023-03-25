481fea0f00a8f54d148e3c4d52165c76ed0623d7
scarfacedotcom <peteradaaku@gmail.com>
2023-03-25 11:27:23 +0100

    few changes

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -2,6 +2,16 @@
 pragma solidity ^0.8.17;
 
 contract Storage {
-    uint256 x = 60;
-    uint256 y = 70;
+    uint256 x = 60; //0x1
+    uint256 y = 70; //0x2
+    mapping (uint => uint) testing; //0x3
+
+    constructor(){
+        // keccak256(20 + 0x3)
+        testing[20] = 76;
+
+        // keccak256(40 + 0x3)
+        testing[40] = 89;
+
+    }
 }
--- scripts/lookup.js
+++ scripts/lookup.js
@@ -1,6 +1,6 @@
 const hre = require("hardhat");
 
-const address = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const address = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 async function lookup() {
     const value = await hre.ethers.provider.getStorageAt(address, "0x0");
